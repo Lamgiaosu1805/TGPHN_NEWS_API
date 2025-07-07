f49df3a9416feb1bbb99ae3a6696da8ed98b7626
Lamgs <nghiemkhaclam185.mta@gmail.com>
2025-07-07 11:42:20 +0700

    add cors

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express')
 const app = express()
 const route = require('./src/routes')
 const morgan = require('morgan')
+const cors = require('cors');
 
 require('dotenv').config();
 
 //use middlewares
 app.use(morgan('dev'))
+app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({
     extended: true
