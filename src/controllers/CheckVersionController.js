c49b3be3e2f42c2946a7c32465bc478660986100
Lâm GS <nghiemkhaclam185.mta@gmail.com>
2026-05-15 18:23:03 +0700

    update version

--- src/controllers/CheckVersionController.js
+++ src/controllers/CheckVersionController.js
@@ -1,7 +1,7 @@
 const CheckVersionController = {
     getCurrentVersionApp: (req, res, next) => {
         try {
-            let versionApp = "1.1.0"
+            let versionApp = "1.1.3"
             const appName = "TGPHN"
             res.json({
                 version: versionApp,
